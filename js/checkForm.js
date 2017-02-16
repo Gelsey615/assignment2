b60f142548000be79ebda5caa9fed4090d3057ec
Gelsey615 <xig37@pitt.edu>
2017-02-15 23:23:06 -0500

    Update checkForm.js

--- js/checkForm.js
+++ js/checkForm.js
@@ -1,16 +1,16 @@
 function checkForm(){
    var inputs = document.getElementsByTagName("input");
-	 for(var i = 0; i < inputs.length-1; i++)
+   for(var i = 0; i < inputs.length-1; i++)
    {
-     if(inputs[i].value==("")){
-        alert("Please enter a value for the required field " + inputs[i].name);
-        return false;
-     }
-		 if(inputs[i].name==("Gender")&&inputs[i].value!=("Female")&&inputs[i].value!=("Male")){
+      if(inputs[i].value==("")){
+         alert("Please enter a value for the required field " + inputs[i].name);
+         return false;
+      }
+      if(inputs[i].name==("Gender")&&inputs[i].value!=("Female")&&inputs[i].value!=("Male")){
         alert("Please enter Female or Male for " + inputs[i].name);
         return false;
-     }
-	 }
+      }
+   }
    alert("Thanks for completing the survey!");
    return true;
 }
